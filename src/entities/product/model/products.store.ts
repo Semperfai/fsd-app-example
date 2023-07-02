d6f7258288771023bbb1ad7d6f2a7fb6ad364b54
Svyatoslav Matyuha <svyatoslavmatyuha@gmail.com>
2023-07-02 21:50:31 +0300

    refactor: products store

--- src/entities/product/model/products.store.ts
+++ src/entities/product/model/products.store.ts
@@ -1,39 +1,36 @@
-import { type ProductDto } from './../api/types';
-import { defineStore } from 'pinia';
-import { getProductsList , getProductById  } from '../api/productApi';
-import { type GetProductByIdParams  } from '../api/productApi';
-import { ref } from 'vue';
+import { type ProductDto } from './../api/types'
+import { defineStore } from 'pinia'
+import { getProductsList, getProductById } from '../api/productApi'
+import { type GetProductByIdParams } from '../api/productApi'
+import { ref } from 'vue'
 
 export const useProductsStore = defineStore('products', () => {
   const isProductLoading = ref<boolean>(false)
   const productsList = ref<ProductDto[]>([])
-  const productById = ref<ProductDto | null>(null);
 
-  async function getProductListAsync (params:any){
+  async function getProductListAsync(params: any) {
     isProductLoading.value = true
     try {
-        const { data } = await getProductsList(params)
-        productsList.value = [ ...productsList.value, ...data]
+      const { data } = await getProductsList(params)
+      productsList.value = [...productsList.value, ...data]
     } finally {
-    isProductLoading.value = false
+      isProductLoading.value = false
     }
   }
-  
-  async function getProductByIdAsync({productId,...params}:GetProductByIdParams) {
+
+  async function getProductByIdAsync({ productId, ...params }: GetProductByIdParams) {
     isProductLoading.value = true
     try {
-        const { data } = await getProductById({productId,...params })
-        productById.value = data
+      const { data } = await getProductById({ productId, ...params })
+      return data
     } finally {
-    isProductLoading.value = false
+      isProductLoading.value = false
     }
   }
 
   return {
     getProductListAsync,
     getProductByIdAsync,
-    productsList,
-    productById  
-  };
-});
-
+    productsList
+  }
+})
